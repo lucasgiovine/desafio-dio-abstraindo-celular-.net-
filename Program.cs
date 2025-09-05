58ae7642504dea0db2b6f395a9f49e34d033a391
lucasgiovine <lucas.giovine23@gmail.com>
2025-09-05 17:15:45 -0300

    realizando TODO's

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone("123", "xr", "123", 16);
+Smartphone nokia = new Nokia("456", "3310", "456", 8);
+
+Console.WriteLine("Testando Iphone:");
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+Console.WriteLine("");
+
+Console.WriteLine("Testando Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("Twitter");
